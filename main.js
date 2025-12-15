3595f3b7987983bd62611f8a1748a0d6044b2fde
PlayerNFB <nicolasfuelbotina@gmail.com>
2025-12-15 15:07:59 -0500

    swiper

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+
+  new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+      delay: 6000,
+      disableOnInteraction: false
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
